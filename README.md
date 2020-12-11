72363f36bc50a90ed15ac69cd6caf6caeaf81d3b
Chris <free2737@gmail.com>
2020-12-11 14:04:23 +0800

    Update code and README

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # binance-import
-## NOT FINISH
-A tool for importing binance exchange information to gekko
+A tool for importing binance exchange information to Gekko, an open source crypto trading bot.
+
+## Gekko
+For details about Gekko please visit [gekko repo](https://github.com/askmike/gekko). Gekko is not maintained anymore, *use it at your own risk*.
+
+## Install
+copy this project to your machine
+
+```git clone https://github.com/i01er/binance-import.git```
+
+run binance-update.py
+
+```python binance-update.py```
+
+copy the output file `binance-markets.json` to replace the original one at `gekko/exchange/wrappers/`
+
+Or you can directly run this script at `gekko/exchange/wrappers/`
+
+Done
+
+### *--Use it at your own risk!--*
+
+## Support
+If you think this helped you in any way, you can buy me a coffee:)
+
+BTC: 19RstSe576DvBNPwVZHqESorZLnBYuqoy3
+
+ETH: 0x3691cedbb55666cb60e68cf1848cc32786e21f40
+
+XMR: 49Ur3QcAMokf2qVBraR83pCAEY17sY6eCjNRHanYZNyMgKqw4ooKtYc1BGRxWMzd9gDP86FyL5zyP6SaN3GwmV6AANMT1yF
--- binance-update.py
+++ binance-update.py
@@ -2,8 +2,8 @@ import json
 import requests
 
 def main():
-    # importData()
-    formatting()
+    importData()
+    formatData()
 
 def importData():
     InfoFile = open("exchangeInfo.json","w")
@@ -19,17 +19,52 @@ def importData():
     InfoFile.write(response.text)
     InfoFile.close()
 
-def formatting():
+def formatData():
     DataFile = open("exchangeInfo.json","r")
     print("Formatting data to fit gekko binance-markets.json")
     RawData = DataFile.read()
     Data = json.loads(RawData)
     Symbols = Data['symbols']
     sym_len = len(Symbols) - 1
+    count = 0
+    _currencies = []
+    _assets = []
+    _markets = []
 
-    while sym_len>0:
-        print(Symbols[sym_len]['symbol'])
-        sym_len = sym_len - 1
+    while count < sym_len:
+        _pair = []
+        _minimalOrder = {
+            "amount": 0,
+            "price": 0,
+            "order": 0
+        }
+        # print(Symbols[count]['filters'][2]['minQty'])
+        if not Symbols[count]['baseAsset'] in _assets:
+            _assets.append(Symbols[count]['baseAsset'])
+        _pair.append(Symbols[count]['baseAsset'])
+        if not Symbols[count]['quoteAsset'] in _currencies:
+            _currencies.append(Symbols[count]['quoteAsset'])
+        _pair.append(Symbols[count]['quoteAsset'])
+        _minimalOrder['amount'] = float(Symbols[count]['filters'][2]['minQty'])
+        _minimalOrder['price'] = float(Symbols[count]['filters'][0]['minPrice'])
+        _minimalOrder['order'] = float(Symbols[count]['filters'][3]['minNotional'])
+        _obj = {
+            "pair": _pair,
+            "minimalOrder": _minimalOrder
+        }
+        _markets.append(_obj)
+        count += 1
+
+    DataOutput = {
+        "assets": _assets,
+        "currencies": _currencies,
+        "markets": _markets
+    }
+
+    OutputFile = open("binance-markets.json", "w")
+    OutputFile.write(json.dumps(DataOutput, sort_keys=True, indent=4))
+    OutputFile.close()
+    print("Done.")
 
 if __name__ == "__main__":
     main()
